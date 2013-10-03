9e94698fb36e79f051806437483465e767a0f4c6
Richard Rodger <richard@ricebridge.com>
2013-10-03 16:27:57 +0100

    seneca-settings

--- mvp-app.js
+++ mvp-app.js
@@ -30,43 +30,12 @@ seneca.use('project')
 
 seneca.use('settings')
 
-/*
-seneca.add('role:entity, cmd:load, name:user, base:sys', function(args,done){
-  console.log('LOAD USER')
-  this.prior(args,done)
-})
-*/
-
-/*
-var userent = seneca.make('sys/user')
-seneca.add('role:user, cmd:login', function(args,done){
-  var seneca = this
-
-  userent.load$({nick:args.nick}, function(err,user){
-    if( err ) return done(err);
-    if( user ) return seneca.prior(args,done);
-
-    seneca.act('role:user, cmd:register', {nick:args.nick,active:true}, function(err,out){
-      if( err ) return done(err);
-      if( !out.ok ) return done(null,out);
-
-      args.user = out.user
-      return seneca.prior(args,done);
-    })
-  })
-})
-
-seneca.add('role:user, cmd:verify_password', function(args,done){
-  done(null,{ok:true})
-})
-*/
 
 
 seneca.ready(function(err){
-  if( err ) { 
-    console.error(err.message)
-    process.exit(1)
-  }
+  if( err ) return process.exit( !console.error(err) );
+
+  var options = seneca.export('options')
 
   var u = seneca.pin({role:'user',cmd:'*'})
   var projectpin = seneca.pin({role:'project',cmd:'*'})
@@ -79,29 +48,9 @@ seneca.ready(function(err){
   u.register({nick:'a1',name:'na1',email:'a1@example.com',password:'a1',active:true,admin:true})
 
 
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{a:{"type":"text", "nice":"A", "help":"Example of text."}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{b:{"type":"email", "nice":"B", "help":"Example of email."}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{c:{"type":"tel", "nice":"C", "help":"Example of tel."}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{d:{"type":"number", "nice":"D", "help":"Example of number."}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{e:{"type":"time", "nice":"E", "help":"Example of time."}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{f:{"type":"date", "nice":"F", "help":"Example of date."}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{g:{"type":"datetime", "nice":"G", "help":"Example of datetime."}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{h:{"type":"color", "nice":"H", "help":"Example of color."}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{i:{"type":"url", "nice":"I", "help":"Example of url."}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{j:{"type":"checkbox", "nice":"J", "help":"Example of checkbox."}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{k:{"type":"range", "nice":"K", "help":"Example of range.", "default" : 50}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{l:{"type":"rating", "nice":"L", "help":"Example of rating.", "stars" : 6 }}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{ll:{"type":"rating", "nice":"LL", "help":"Example of rating."}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{m:{"type":"yesno", "nice":"M", "help":"Example of yesno."}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{n:{"type":"onoff", "nice":"N", "help":"Example of onoff slider.", "default" : 0}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{o:{"type":"selectbuttons", "nice":"O", "help":"Example of selectbuttons.", "options" : ["foo", "bar", "baz"]}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{p:{"type":"selectdropdown", "nice":"P", "help":"Example of selectdropdown.", "options" : ["foo", "bar", "baz"]}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{q:{"type":"selectdropdownplus", "nice":"Q", "help":"Example of selectdropdownplus.", "options" : ["foo", "bar", "baz"]}}')
-  seneca.act('role:settings, cmd:define_spec, kind:user, spec:{r:{"type":"longtext", "nice":"R", "help":"Example of longtext."}}')
+  seneca.act('role:settings, cmd:define_spec, kind:user',{spec:options.settings.spec})
 
-
-  var options = seneca.export('options')
-  var web     = seneca.export('web')
+  var web = seneca.export('web')
 
   var app = express()
 
--- options.example.js
+++ options.example.js
@@ -21,5 +21,30 @@ module.exports = {
         pass: "yourpass"
       }
     }
+  },
+
+  settings: {
+    spec: {
+      a:{"type":"text", "nice":"A", "help":"Example of text."},
+      b:{"type":"email", "nice":"B", "help":"Example of email."},
+      c:{"type":"tel", "nice":"C", "help":"Example of tel."},
+      d:{"type":"number", "nice":"D", "help":"Example of number."},
+      e:{"type":"time", "nice":"E", "help":"Example of time."},
+      f:{"type":"date", "nice":"F", "help":"Example of date."},
+      g:{"type":"datetime", "nice":"G", "help":"Example of datetime."},
+      h:{"type":"color", "nice":"H", "help":"Example of color."},
+      i:{"type":"url", "nice":"I", "help":"Example of url."},
+      j:{"type":"checkbox", "nice":"J", "help":"Example of checkbox."},
+      k:{"type":"range", "nice":"K", "help":"Example of range.", "default" : 50},
+      l:{"type":"rating", "nice":"L", "help":"Example of rating.", "stars" : 6 },
+      ll:{"type":"rating", "nice":"LL", "help":"Example of rating."},
+      m:{"type":"yesno", "nice":"M", "help":"Example of yesno."},
+      n:{"type":"onoff", "nice":"N", "help":"Example of onoff slider.", "default" : 0},
+      o:{"type":"selectbuttons", "nice":"O", "help":"Example of selectbuttons.", "options" : ["foo", "bar", "baz"]},
+      p:{"type":"selectdropdown", "nice":"P", "help":"Example of selectdropdown.", "options" : ["foo", "bar", "baz"]},
+      q:{"type":"selectdropdownplus", "nice":"Q", "help":"Example of selectdropdownplus.", "options" : ["foo", "bar", "baz"]},
+      r:{"type":"longtext", "nice":"R", "help":"Example of longtext."},
+    }
   }
+
 }
